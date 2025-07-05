823d0fdd2353970988b8e6bd0220bb5ba23b89c5
Neeraj <neera.link@gmail.com>
2025-07-05 13:20:43 +0530

    correct asgi wsgi manage

--- shynecast/wsgi.py
+++ shynecast/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rainorshine project.
+WSGI config for shynecast project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rainorshine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shynecast.settings')
 
 application = get_wsgi_application()
